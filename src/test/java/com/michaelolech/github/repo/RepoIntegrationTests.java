dedd4f60f764a7632c310bbc9447092906cea590
micavanco <micavanco@gmail.com>
2026-01-21 18:13:12 +0100

    feat(app): fix tests to use rest test client and make a http request to API

--- src/test/java/com/michaelolech/github/repo/RepoIntegrationTests.java
+++ src/test/java/com/michaelolech/github/repo/RepoIntegrationTests.java
@@ -4,19 +4,39 @@ import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.client.EntityExchangeResult;
+import org.springframework.test.web.servlet.client.RestTestClient;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @WireMockTest(httpPort = 8081)
+@AutoConfigureRestTestClient
 public class RepoIntegrationTests {
 
     @Autowired
-    private RepoService repoService;
+    RestTestClient restTestClient;
 
     @Test
     public void gettingReposWithCorrectUsername_shouldReturnProperListOfRepositories() {
-        var repos = repoService.getRepos("test");
+        EntityExchangeResult<Repo[]> result = restTestClient.get()
+                .uri("/api/v1/repos/test")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(Repo[].class)
+                .returnResult();
+
+        if (result.getResponseBody() == null) {
+            Assertions.fail();
+        }
+
+        List<Repo> repos = List.of(result.getResponseBody());
 
         Assertions.assertNotNull(repos);
         Assertions.assertEquals(2, repos.size());
@@ -28,14 +48,22 @@ public class RepoIntegrationTests {
 
     @Test
     public void gettingReposWithIncorrectUsername_shouldThrowException() {
-        try {
-            repoService.getRepos("incorrect_test");
-            Assertions.fail("Should have thrown exception");
-        } catch (UserNotFoundException e) {
-            Assertions.assertEquals("User not found.", e.getMessage());
-            Assertions.assertEquals(HttpStatusCode.valueOf(404), e.getStatus());
-        } catch (Exception e) {
-            Assertions.fail("Should have thrown exception");
+        EntityExchangeResult<UserNotFoundResponse> result = restTestClient.get()
+                .uri("/api/v1/repos/incorrect_test")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(UserNotFoundResponse.class)
+                .returnResult();
+
+        if (result.getResponseBody() == null) {
+            Assertions.fail();
         }
+
+        var responseBody = result.getResponseBody();
+
+        Assertions.assertEquals("User not found.", responseBody.message());
+        Assertions.assertEquals(HttpStatus.NOT_FOUND.value(), responseBody.status());
     }
 }
