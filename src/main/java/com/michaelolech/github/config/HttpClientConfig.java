23e459c52f12128dfc503ba14c45fada392c1377
micavanco <micavanco@gmail.com>
2026-01-21 19:16:20 +0100

    feat(app): add proper error handling and inject custom http client

--- src/main/java/com/michaelolech/github/config/HttpClientConfig.java
+++ src/main/java/com/michaelolech/github/config/HttpClientConfig.java
@@ -1,9 +1,11 @@
 package com.michaelolech.github.config;
 
 import com.michaelolech.github.repo.RepoHttpClient;
+import com.michaelolech.github.repo.UserNotFoundException;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.service.registry.ImportHttpServices;
 
@@ -16,6 +18,24 @@ public class HttpClientConfig {
     RestClient restClient(RestClient.Builder builder, RepoBaseUrl urlProperties) {
         return builder
                 .baseUrl(urlProperties.baseUrl())
+                .defaultStatusHandler(
+                        status -> status.value() == 404,
+                        (_, res) -> {
+                            throw new UserNotFoundException(res.getStatusCode(), "User not found.");
+                        }
+                )
+                .defaultStatusHandler(
+                        HttpStatusCode::is4xxClientError,
+                        (_, _) -> {
+                            throw new RuntimeException("Resource not available.");
+                        }
+                )
+                .defaultStatusHandler(
+                        HttpStatusCode::is5xxServerError,
+                        (_, _) -> {
+                            throw new RuntimeException("Remote service not available.");
+                        }
+                )
                 .build();
     }
 }
--- src/main/java/com/michaelolech/github/repo/RepoClient.java
+++ src/main/java/com/michaelolech/github/repo/RepoClient.java
@@ -1,59 +1,35 @@
 package com.michaelolech.github.repo;
 
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class RepoClient {
-    private final RestClient restClient;
+    private final RepoHttpClient restClient;
 
-    public RepoClient(RestClient restClient) {
+    public RepoClient(RepoHttpClient restClient) {
         this.restClient = restClient;
     }
 
     public List<GitHubRepo> getListOfRepositories(final String username) {
-        return restClient.get()
-                .uri("/users/{username}/repos", username)
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange((_, response) -> {
-                    if (response.getStatusCode().isSameCodeAs(HttpStatusCode.valueOf(404))) {
-                        throw new UserNotFoundException(response.getStatusCode(), "User not found.");
-                    } else if (response.getStatusCode().is4xxClientError()) {
-                        throw new RuntimeException("Resource not available.");
-                    }
-                    else {
-                        GitHubRepo[] repos = response.bodyTo(GitHubRepo[].class);
+        GitHubRepo[] repos = restClient.getUserRepos(username);
 
-                        if (repos == null) {
-                            throw new RuntimeException("Cannot parse response.");
-                        }
+        if (repos == null) {
+            throw new RuntimeException("Cannot parse response.");
+        }
 
-                        return List.of(repos);
-                    }
-                });
+        return Arrays.asList(repos);
     }
 
     public List<GitHubBranch> getListOfBranches(final String username, final String repoName) {
-        return restClient.get()
-                .uri("/repos/{username}/{repoName}/branches", username, repoName)
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange((_, response) -> {
-                    if (response.getStatusCode().is4xxClientError()) {
-                        throw response.createException();
-                    }
-                    else {
-                        GitHubBranch[] branches = response.bodyTo(GitHubBranch[].class);
+        GitHubBranch[] branches = restClient.getUserRepoBranches(username, repoName);
 
-                        if (branches == null) {
-                            throw new RuntimeException("Cannot parse response.");
-                        }
+        if (branches == null) {
+            throw new RuntimeException("Cannot parse response.");
+        }
 
-                        return List.of(branches);
-                    }
-                });
+        return Arrays.asList(branches);
     }
 }
